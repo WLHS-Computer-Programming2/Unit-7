ab10fe3f4d69e295b9bbaa5686f6463936e918b5
smithbwlhs <smithb@wlwv.k12.or.us>
2024-03-20 14:42:08 -0700

    done

--- Period_8/file_reader.py
+++ Period_8/file_reader.py
@@ -6,7 +6,7 @@ From Python Crash Course 3rd edition
 from pathlib import Path
 
 def main():
-    path = Path('Period_8\pi_digits.txt')
+    path = Path('Period_8\million_digits_pi.txt')
 
     # # access all content of a file
     # contents = path.read_text().strip()
@@ -19,8 +19,7 @@ def main():
     pi_string =''
     for line in lines:
         pi_string += line.strip()
-    print(pi_string)
-    print(f"This is pi to {len(pi_string)-2} places.")
+    print(f"{pi_string[:52]}...")
 
     # Ask a user for their birthday in mmddyy form
     # Tell them if their birthday is in the first n digits of pi
@@ -35,7 +34,8 @@ def main():
             print(f"Incorrect form, try again")
             continue
         if user_bday in pi_string:
-            print(f"Your birthday is in the first {len(pi_string)-2} digits of pi")
+            value = len(pi_string)-2
+            print(f"Your birthday is in the first {value:,} digits of pi")
         else:
             print(f"Sorry, your birthday is not in the first {len(pi_string)-2} digits of pi")
 if __name__ == '__main__':
